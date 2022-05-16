1ba8d4d49d03154e6d86f4836eeb6bfbb8174c41
Mannar Hielal <mannar.hielal@liip.ch>
2022-05-16 11:54:21 +0200

    Implement save to localstorage

--- script.js
+++ script.js
@@ -2,21 +2,33 @@ const input = document.getElementById("input");
 const list = document.getElementById("todos");
 const form = document.getElementById("form");
 
+const todosFromLocalStorage = JSON.parse(localStorage.getItem("todos"));
 
+if (todosFromLocalStorage) {
+    todosFromLocalStorage.forEach(el => addTodo(el));
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     addTodo();
 });
 
 
-function addTodo() {
+function addTodo(savedTodo) {
     let todoText = input.value;
+    let todoClass = "";
+
+    if (savedTodo) {
+        todoText = savedTodo.text;
+        todoClass = savedTodo.completed ? "completed" : "";
+    }
+
     if (todoText !== '') {
 
         // create li
         const todoEl = document.createElement('li');
+        todoEl.className = `${todoClass}`;
+
         const todoSpan = document.createElement('span');
         todoSpan.className = "text";
         todoSpan.innerText = todoText;
@@ -38,16 +50,36 @@ function addTodo() {
         // implement "completed"
         checkBtn.addEventListener("click", e => {
             e.target.parentElement.classList.toggle("completed");
+            updateLocalStorage()
         });
 
         // implement "deleted"
         deleteBtn.addEventListener("click", e => {
-            console.log(e.target);
-            const li = e.target.parentElement;
-            const ul = li.parentElement;
-            if (li.classList.contains("completed")) {
-                ul.removeChild(li);
+            if (confirm("Do you really want to delete?")) {
+                const li = e.target.parentElement;
+                const ul = li.parentElement;
+                if (li.classList.contains("completed")) {
+                    ul.removeChild(li);
+                }
+                updateLocalStorage()
             }
         });
+
+        updateLocalStorage();
     }
+}
+
+function updateLocalStorage() {
+    // construct an array from todos li
+    todoElements = document.querySelectorAll("li");
+    const todosArr = [];
+
+    Array.from(todoElements).map(el => {
+        todosArr.push({
+            text: el.innerText,
+            completed: el.classList.contains("completed")
+        })
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todosArr));
 }
